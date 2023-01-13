155ede76a5759633ad6accc6ecfe227e97d6fa4f
Rayannecristinar <rayanne.cristinar@gmail.com>
2023-01-12 21:38:23 -0300

    Criação do site do github

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData(){
-    const url = ''
+    const url = 'https://raw.githubusercontent.com/Rayannecristinar/DIO-Portfolio-Profissional/main/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
